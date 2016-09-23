1a8c8fd3509f655d715d79de3c5d916f43a005c2
MrCooper42 <mr.cooper42co@gmail.com>
2016-09-23 14:35:56 -0600

    new commit

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,7 @@ const humps = require('humps');
 const router = express.Router();
 
 router.post('/', (req, res, next) => {
+
   const email = req.body.email;
   const password = req.body.password;
 
